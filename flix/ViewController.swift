663b7c5c0b70decd129fba4d7c4e80087ba0c40f
ahmedkadd <akadd8@gmail.com>
2021-02-21 23:24:17 -0800

    Populate movie details view

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -63,6 +63,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+    
     }
 }
